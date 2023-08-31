c519fc4448aad3b04b5a0532225097ccd87c8430
agastyavinchhi <agastya.vinchhi@gmail.com>
2023-08-30 20:51:16 -0500

    Initall Commit

---
+++ old.py
@@ -0,0 +1,11 @@
+from dotenv import load_dotenv
+import os
+load_dotenv
+
+#Gather data from env
+client_id = os.getenv("CLIENT_ID")
+client_secret = os.getenv("CLIENT_SECRET")
+
+def print_function():
+    pass
+
