81f8816a78f98827e71b0185a2d5b6223f74d263
agastyavinchhi <agastya.vinchhi@gmail.com>
2023-09-04 13:50:30 -0500

    New changes with additional functions
    Added new functions to parse data sucessfuly. Now data can easily be converted to word embeddings and hence, stored on Pinecone

--- main.py
+++ main.py
@@ -1,72 +1,50 @@
-from dotenv import load_dotenv
 import os
-load_dotenv
-import spotipy
-import spotipy.util as util
-import spotipy.oauth2 as oauth2
-import requests
-import pandas
+import json
+
+#to stop opening the file again and again - we can open it once here and make it global (optimize)
+def file_open(playlist_name = "playlist1.json"):
+    f = open("playlist1.json")
+    global data
+    data = json.load(f)
+    f.close()
+    return data
 
 
+#user needs to input the file name with all their spotify data (default spotify data is given as "playlist1.json")
+def data_parse(): 
+    global playlist_names
+    global playlist_dict
+    playlist_names = []
+    playlist_dict = {}
+    
+    try:
+        for i in data["playlists"]:
+            playlist_i_name = i["name"]
+            playlist_names.append(playlist_i_name)
+            playlist_tracks = []
+            for track_list in i["items"]:
+                track_i_name = track_list["track"]["trackName"]
+                playlist_tracks.append(track_i_name)
+            playlist_dict[playlist_i_name] = playlist_tracks
+                
+    except:
+        print("Error while parsing data. Check for the error.")
+    return playlist_dict
 
-#Gather data from env
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-username = '31hr6som5zbwu62glxe32kcrjeva' #enter your username
-redirect_uri = 'http://localhost:7777/callback'
-playlist_id = '2by4lAbCKZJI0p7jRJV6Rn?si=811e58f35940495a'
-uri = 'spotify:playlist:playlist_id'
+#user needs to input the playlist name that they prefer. #this function instantly prints every song in the user's playlist
+def playlist_request(playlist_request):
+    for name in playlist_names:
+        try:
+            if name in playlist_request:
+                for playlist_name in playlist_dict:
+                    if name in playlist_name:
+                        global playlist_search
+                        playlist_search = {}
+                        playlist_search[name] = playlist_dict[name]
+        except:
+            print(f"Sorry! There is no playlist named {playlist_request} in your library! Please check again and find a valid playlist. Make sure to look for any special characters or spaces in between the words of your playlists")
+    return playlist_search
+          
 
-
-def details_print():
-    return client_id,client_secret
-
-def generate_token():
-    credentials = oauth2.SpotifyClientCredentials(
-        client_id=client_id,
-        client_secret=client_secret)
-    token = credentials.get_access_token()
-    return token
-
-token = generate_token()
-spotify = spotipy.Spotify(auth=token)
-
-def get_playlist(username=username, playlist_id=playlist_id):
-    results = spotify.user_playlist(username)
-    return results
-
-
-# def write_tracks(text_file, tracks):
-#     with open(text_file, 'a') as file_out:
-#         while True:
-#             for item in tracks['items']:
-#                 if 'track' in item:
-#                     track = item['track']
-#                 else:
-#                     track = item
-#                 try:
-#                     track_url = track['external_urls']['spotify']
-#                     file_out.write(track_url + '\n')
-#                 except KeyError:
-#                     print(u'Skipping track {0} by {1} (local only?)'.format(
-#                             track['name'], track['artists'][0]['name']))
-#             # 1 page = 50 results
-#             # check if there are more pages
-#             if tracks['next']:
-#                 tracks = spotify.next(tracks)
-#             else:
-#                 break
-
-
-# def write_playlist(username, playlist_id):
-#     results = spotify.user_playlist(username, playlist_id,
-#                                     fields='tracks,next,name')
-#     text_file = u'{0}.txt'.format(results['name'], ok='-_()[]{}')
-#     print(u'Writing {0} tracks to {1}'.format(
-#             results['tracks']['total'], text_file))
-#     tracks = results['tracks']
-#     write_tracks(text_file, tracks)
-
-
-# token = generate_token()
-# spotify = spotipy.Spotify(auth=token)
+            
+            
